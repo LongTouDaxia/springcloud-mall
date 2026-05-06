2528ccc3f0d6018a3ff5f2035d3b9b1bf17eae2e
LongTouDaxia <15549842+longtoudaxia@user.noreply.gitee.com>
2026-05-06 14:32:54 +0800

    项目结构初始化

---
+++ mall-api/src/test/java/com/longtou/mallapi/MallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longtou.mallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
